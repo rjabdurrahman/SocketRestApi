80e20fe896713ae1030955b901dc5e046b2b69eb
rjabdurrahman <rj.abdurrahman@gmail.com>
2019-08-26 17:44:44 +0600

    Working: socket io

---
+++ server.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var bodyParser = require('body-parser')
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var mongoose = require('mongoose');
+
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
+
+var Message = mongoose.model('Message',{
+  name : String,
+  message : String
+})
+
+var db = 'mongodb+srv://rjrahman:rahman09@startup-vn2jf.mongodb.net/test?retryWrites=true&w=majority'
+
+app.get('/messages', (req, res) => {
+  Message.find({},(err, messages)=> {
+    res.send(messages);
+  })
+})
+
+app.post('/messages', (req, res) => {
+  var message = new Message(req.body);
+  message.save((err) =>{
+    if(err)
+      sendStatus(500);
+    io.emit('message', req.body);
+    res.sendStatus(200);
+  })
+})
+
+io.on('connection', () =>{
+  console.log('a user is connected')
+})
+
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
+
+var server = http.listen(3001, () => {
+  console.log('server is running on port', server.address().port);
+});
